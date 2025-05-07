36f566dda9396bcce3e80e382610e62e7c9b7237
leodan2702 <21690080@tecvalles.mx>
2025-05-06 20:57:06 -0600

    adjust logger configuration and formatting

--- cryptomesh/log/logger.py
+++ cryptomesh/log/logger.py
@@ -1,16 +1,8 @@
-# cryptomesh/log/logger_config.py
-
 import logging
-from cryptomesh.config import (
-    CRYPTO_MESH_DEBUG,
-    CRYPTO_MESH_LOG_PATH,
-    CRYPTO_MESH_LOG_LEVEL,
-    CRYPTO_MESH_LOG_ROTATION_WHEN,
-    CRYPTO_MESH_LOG_ROTATION_INTERVAL,
-    CRYPTO_MESH_LOG_TO_FILE,
-    CRYPTO_MESH_LOG_ERROR_FILE,
-)
 from cryptomesh.log import Log
+from cryptomesh import config
+
+CRYPTO_MESH_DEBUG = config.CRYPTO_MESH_DEBUG
 
 def console_handler_filter(record: logging.LogRecord):
     if CRYPTO_MESH_DEBUG:
@@ -20,15 +12,8 @@ def console_handler_filter(record: logging.LogRecord):
 def get_logger(name: str):
     return Log(
         name=name,
-        level=getattr(logging, CRYPTO_MESH_LOG_LEVEL.upper(), logging.DEBUG),
-        path=CRYPTO_MESH_LOG_PATH,
-        console_handler_filter=console_handler_filter,
-        to_file=CRYPTO_MESH_LOG_TO_FILE,
-        error_log=CRYPTO_MESH_LOG_ERROR_FILE,
-        when=CRYPTO_MESH_LOG_ROTATION_WHEN,
-        interval=CRYPTO_MESH_LOG_ROTATION_INTERVAL,
+        console_handler_filter=console_handler_filter
     )
 
 # Logger global opcional
-L = get_logger("cryptomesh")
-
+L = get_logger("cryptomesh") 
