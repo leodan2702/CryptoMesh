03d244ab7fdb8d053478bc9f44e00268e697901a
leodan2702 <21690080@tecvalles.mx>
2025-09-11 22:59:25 -0600

    Add CryptoMeshError class with to_http_exception and from_exception methods

--- cryptomesh/errors.py
+++ cryptomesh/errors.py
@@ -1,3 +1,9 @@
+from fastapi import HTTPException
+from typing import Any, Callable
+from functools import wraps
+
+
+
 class CryptoMeshError(Exception):
     def __init__(self, message: str, code: int = 500):
         self.message = message
@@ -9,6 +15,21 @@ class CryptoMeshError(Exception):
             "message": self.message,
             "code": self.code
         }
+
+    def to_http_exception(self):
+        """
+        Convert to FastAPI HTTPException
+        """
+        return HTTPException(status_code=self.code, detail=self.to_dict())
+
+    @staticmethod
+    def from_exception(e: Exception)-> "CryptoMeshError":
+        """
+        Convert a generic exception to a CryptoMeshError
+        """
+        if isinstance(e, CryptoMeshError):
+            return e
+        return CryptoMeshError(message=str(e), code=500)
 
 
 class FunctionNotFound(CryptoMeshError):
@@ -40,3 +61,19 @@ class CreationError(CryptoMeshError):
     def __init__(self, entity_type: str, entity_id: str, original_exception: Exception):
         message = f"Error creating {entity_type} '{entity_id}': {str(original_exception)}"
         super().__init__(message=message, code=500)
+
+
+# Decorator
+def handle_crypto_errors(func: Callable) -> Callable:
+    """
+    Decorator to handle CryptoMeshError exceptions in FastAPI endpoints
+    """
+    @wraps(func)
+    async def wrapper(*args, **kwargs) -> Any:
+        try:
+            return await func(*args, **kwargs)
+        except CryptoMeshError as e:
+            raise e.to_http_exception()
+        except Exception as e:
+            raise CryptoMeshError.from_exception(e).to_http_exception()
+    return wrapper
