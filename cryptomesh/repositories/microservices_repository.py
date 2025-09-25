420713d5209189e4ebd46eddee108e9f79315f88
Leo Garcia <21690080@tecvalles.mx>
2025-09-25 16:00:44 -0600

    fix(repositories): correct get_by_filter and return typing

--- cryptomesh/repositories/microservices_repository.py
+++ cryptomesh/repositories/microservices_repository.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorCollection
 from cryptomesh.models import MicroserviceModel
 from cryptomesh.repositories.base_repository import BaseRepository
-from typing import Optional
+from typing import Optional, List
 
 class MicroservicesRepository(BaseRepository[MicroserviceModel]):
     def __init__(self, collection: AsyncIOMotorCollection):
@@ -10,4 +10,11 @@ class MicroservicesRepository(BaseRepository[MicroserviceModel]):
     async def get_by_id(self, microservice_id: str, id_field: str = "microservice_id") -> Optional[MicroserviceModel]:
         document = await self.collection.find_one({"microservice_id": microservice_id})
         return MicroserviceModel(**document) if document else None
+
+    async def get_by_filter(self, filter: dict) -> List[MicroserviceModel]:
+        docs = []
+        cursor = self.collection.find(filter)
+        async for doc in cursor:
+            docs.append(ActiveObjectModel(**doc))
+        return docs
 
