e2dee21b6fdbf94a6863cfc9cdf03d4afa4acb16
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2025-09-27 15:09:17 -0600

    Cryptomesh healthcheck - fixed again 3

--- wait_cryptomesh.sh
+++ wait_cryptomesh.sh
@@ -2,7 +2,7 @@
 URL=$1
 echo "Waiting for service at $URL..."
 until $(curl --output /dev/null --silent --head --fail "$URL"); do
-    printf '.'
+    echo '$URL is not available yet. Retrying in 5 seconds...'
     sleep 5
 done
 echo "Service is up!"
